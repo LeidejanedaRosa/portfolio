5f7082730f4fd0b84fd8ab71efa1b768597018d6
Leidejane da Rosa <leidejanedarosa.81@gmail.com>
2025-02-16 20:49:32 -0300

    feat: add asset alias and animations to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
       "./src/**/*.{html,js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          'item-1': 'fadeIn 0.3s ease-out 0s forwards',
+          'item-2': 'fadeIn 0.3s ease-out 0.1s forwards',
+          'item-3': 'fadeIn 0.3s ease-out 0.2s forwards',
+          'item-4': 'fadeIn 0.3s ease-out 0.3s forwards',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: 0, transform: 'scale(0)' },
+            '100%': { opacity: 1, transform: 'scale(1)' },
+          },
+        },
+      },
     },
     plugins: [],
   }
