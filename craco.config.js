f530a981a745676aa0ec84675fb175425c6b2c5a
Leidejane da Rosa <leidejanedarosa.81@gmail.com>
2025-02-16 16:54:01 -0300

    chore: final adjustments to the settings

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+};
