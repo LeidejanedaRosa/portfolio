231855b56ae037d285f389b12c641ee4aa3f536e
Leidejane da Rosa <leidejanedarosa.81@gmail.com>
2025-02-18 21:06:41 -0300

    feat: add support for image modules and CSS styles in TypeScript, update Tailwind settings and dependencies

--- craco.config.js
+++ craco.config.js
@@ -8,5 +8,34 @@ module.exports = {
       "@pages": path.resolve(__dirname, "src/pages"),
       "@components": path.resolve(__dirname, "src/components"),
     },
+    configure: (webpackConfig) => {
+          return webpackConfig;
+    },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true, 
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("tailwindcss"),
+                  require("autoprefixer")
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ], 
   },
 };
